29ed345aad77e870776415bab1a3369751e87c3b
Dumfing <dumpling.d.m@gmail.com>
2018-09-19 02:23:23 -0400

    New homepage is now in place

--- js/sunsetjs.js
+++ js/sunsetjs.js
@@ -1,19 +1,55 @@
 function get_circle_radius(circle_colour){
   //Assumes we're trying to get the radii from the body
   //Assumes units are in vh
-  var style = getComputedStyle(document.body);
-  var measure = style.getPropertyValue("--"+circle_colour+"_radius");
+  var docStyle = getComputedStyle(document.body);
+  var measure = docStyle.getPropertyValue("--"+circle_colour+"_radius");
   var measureInt = parseFloat(measure.substring(0,measure.length-2))/100;
   var unit = measure.substring(measure.length-2) // I have this in case I wish to differentiate between units
   return measureInt*document.documentElement.clientHeight/2 //divide by 2 because width is diameter
 }
 
-function place_logo(logo_id, ring_color){
+function place_logo(logo_id, parent_ring, inner_ring){
   var logo = document.getElementById(logo_id);
-
   //Placing logo in proper x position
-  var x_position = 0;
-  var circle_radius = get_circle_radius(ring_color);
+  var x_position, y_position;
+  var circle_radius = get_circle_radius(parent_ring);
+  var inner_circle_radiuse = get_circle_radius(inner_ring);
   var viewport_width = document.documentElement.clientWidth;
-  
+  var circle_offscreenpixels_left = circle_radius - (viewport_width/2);
+  x_position = Math.max(circle_offscreenpixels_left, 0) + ((circle_radius-inner_circle_radiuse)/2)+(logo.width/2);
+  logo.style.left = (x_position).toString()+"px";
+
+  //Placing logo in proper y position
+  var angle = Math.acos((circle_radius-x_position)/circle_radius);
+  y_position = circle_radius*Math.sin(angle);
+  logo.style.top = (circle_radius-y_position).toString()+"px";
 }
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+
+function place_logos(){
+    place_logo("projects_page", "red", "orange");
+    place_logo("insta_logo", "orange", "yellow");
+    place_logo("github_logo", "yellow", "white");
+
+}
+
+window.onload = function(){place_logos()};
+window.onresize = function(){place_logos()};
